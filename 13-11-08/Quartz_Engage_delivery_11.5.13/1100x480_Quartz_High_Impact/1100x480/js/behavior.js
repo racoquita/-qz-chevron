d643d7dd6f1aa7706dc6a623fe15c03d973c3dcd
Richard Stovall <rstovall@me.com>
2013-11-08 22:21:24 -0500

    adding the chevron v2 ads from the client

---
+++ 13-11-08/Quartz_Engage_delivery_11.5.13/1100x480_Quartz_High_Impact/1100x480/js/behavior.js
@@ -0,0 +1,32 @@
+//Anything you need to do on initiation of the MediaMind code should be put here
+
+//Custom Interaction
+function customInt() {
+	//EB.userActionCounter("InteractionName");
+}
+
+//Clickthrough handler
+function bkndClicked(){
+	EB.clickthrough("bkndclicked");
+}
+
+function tabClicked(){
+	EB.clickthrough("ctaclicked");
+}
+
+function ctaClicked(){
+	EB.clickthrough("ctaclicked");
+}
+
+function rightClicked(){
+	moveRight();
+}
+
+function leftClicked(){
+	moveLeft();
+}
+
+
+
+
+
