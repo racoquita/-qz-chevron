da2fc8bce9994e37f4a798de75f612b2be262615
Richard Stovall <rstovall@me.com>
2014-04-11 13:48:50 -0400

    finalizing

--- energy-economy/14-04-10/desktop/js/app.js
+++ energy-economy/14-04-10/desktop/js/app.js
@@ -2,6 +2,7 @@ var App = function() {
 	var that = this;
 	var slider = null;
 	var current = 0;
+	var timeouts = [];
 
 	this.on = function() {
 		slider = new IScroll('#slider', {
@@ -15,6 +16,34 @@ var App = function() {
     		deceleration: 0.01
 		});
 
+		slider.on('scrollEnd', function(){
+			var page = slider.currentPage.pageX;
+
+			if(page == 1) {
+				timeouts[0] = setTimeout(function(){
+					$('.map-people').show().addClass('bounceIn');
+				}, 250);
+			}
+			if(page == 2) {
+				timeouts[1] = setTimeout(function(){
+					$('.man').show().addClass('bounceIn');
+
+					timeouts[2] = setTimeout(function(){
+						$('.men').show().addClass('bounceIn');
+					}, 250);
+				}, 250);
+			}
+			if(page == 3) {
+				timeouts[3] = setTimeout(function(){
+					$('.slide:nth-child(4) .heading').show().addClass('bounceIn');
+
+					timeouts[4] = setTimeout(function(){
+						$('.cta-inner').css('display', 'block').addClass('bounceIn');
+					}, 250);
+				}, 250);
+			}
+		});
+
 		$('.arrow-right').on('click', function(e){
 			if(current < 3) {
 				slider.goToPage(current + 1, 0, 250, IScroll.utils.easeIn);
@@ -22,6 +51,7 @@ var App = function() {
 				that.handleChange();
 			}
 		});
+
 		$('.arrow-left').on('click', function(e){
 			if(current > 0) {
 				slider.goToPage(current - 1, 0, 250, IScroll.utils.easeIn);
@@ -29,6 +59,16 @@ var App = function() {
 				that.handleChange();
 			}
 		});
+
+		$('.one').on('click', function(){
+			$('.info-one').toggle();
+		});
+
+		$('.two').on('click', function(){
+			$('.info-two').toggle();
+		});
+
+		$('.slide:nth-child(1) .heading').show().addClass('bounceInReverse');
 	}
 	this.off = function() {
 		slider.destroy();
@@ -40,5 +80,6 @@ var App = function() {
 		current == 1 || current == 2 ? $('.cta').show() : $('.cta').hide();
 		$('.dot').removeClass('active');
 		$('.dot:nth-child('+ (current + 1) +')').addClass('active');
+		$('.info-one, .info-two').hide();
 	}
 };
