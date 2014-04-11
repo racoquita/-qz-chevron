b740f7b3020f8720c9618a29aaa2944953bc4a5b
Richard Stovall <rstovall@me.com>
2014-04-11 12:05:41 -0400

    adding dot indicators adding logo and cat adding content to slides

--- energy-economy/14-04-10/desktop/js/app.js
+++ energy-economy/14-04-10/desktop/js/app.js
@@ -17,19 +17,28 @@ var App = function() {
 
 		$('.arrow-right').on('click', function(e){
 			if(current < 3) {
-				slider.goToPage(current + 1, 0, 250, IScroll.utils.ease.quadratic);
+				slider.goToPage(current + 1, 0, 250, IScroll.utils.easeIn);
 				current++;
+				that.handleChange();
 			}
 		});
 		$('.arrow-left').on('click', function(e){
 			if(current > 0) {
-				slider.goToPage(current - 1, 0, 250, IScroll.utils.ease.quadratic);
+				slider.goToPage(current - 1, 0, 250, IScroll.utils.easeIn);
 				current--;
+				that.handleChange();
 			}
 		});
 	}
 	this.off = function() {
 		slider.destroy();
 		slider = null;
+	}
+	this.handleChange = function() {
+		current == 3 ? $('.arrow-right').hide() : $('.arrow-right').show();
+		current == 0 ? $('.arrow-left').hide() : $('.arrow-left').show();
+		$('.dot').removeClass('active');
+		$('.dot:nth-child('+ (current + 1) +')').addClass('active');
+		current == 1 || current == 2 ? $('.cta').show() : $('.cta').hide();
 	}
 };
