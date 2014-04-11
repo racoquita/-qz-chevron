1c3a9500251af130a97b492ee4e73a7dc9846b5c
Richard Stovall <rstovall@me.com>
2014-04-11 11:10:05 -0400

    creating the responsive slider - looking good!

--- energy-economy/14-04-10/desktop/js/app.js
+++ energy-economy/14-04-10/desktop/js/app.js
@@ -1,12 +1,35 @@
 var App = function() {
 	var that = this;
+	var slider = null;
+	var current = 0;
 
 	this.on = function() {
-		$(function() {
-		    $(".rslides").responsiveSlides();
-	  	});
+		slider = new IScroll('#slider', {
+			scrollY: false,
+			scrollX: true,
+			snap: 'li',
+			momentum: false,
+			disableMouse: true,
+    		disablePointer: true,
+    		bounceEasing: 'quadratic',
+    		deceleration: 0.01
+		});
+
+		$('.arrow-right').on('click', function(e){
+			if(current < 3) {
+				slider.goToPage(current + 1, 0, 250, IScroll.utils.ease.quadratic);
+				current++;
+			}
+		});
+		$('.arrow-left').on('click', function(e){
+			if(current > 0) {
+				slider.goToPage(current - 1, 0, 250, IScroll.utils.ease.quadratic);
+				current--;
+			}
+		});
 	}
 	this.off = function() {
-
+		slider.destroy();
+		slider = null;
 	}
 };
