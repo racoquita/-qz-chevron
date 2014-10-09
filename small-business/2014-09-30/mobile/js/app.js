bff7ff0c1e7d412ee802695d72cdc4725ffd0321
Carolina <racoquita@me.com>
2014-10-09 19:07:51 -0400

    chevron Edits mobile small business

--- small-business/2014-09-30/mobile/js/app.js
+++ small-business/2014-09-30/mobile/js/app.js
@@ -15,20 +15,53 @@ var App = function() {
 					delay: 0,
 					duration: 500,
 					complete: function() {
-						that.showAgree();
+						
+						//that.showAgree();
+						$('.headline').velocity({ height: "+=85px" },{ 
+						    delay: 100,
+						    duration: 500,
+						    complete:function(){
+
+						    	$('.headline').velocity({ height: "+=122px" },{
+						    		delay:200,
+						    		duration: 500,
+						    		complete:function(){
+						    			//that.showAgree();
+						    			$('.headline div img').velocity({
+						    				marginTop:"0px"
+						    			}, {
+						    				delay: 100,
+						    				duration: 500,
+						    				complete:function() {
+						    					that.showAgree();
+						    				}
+						    			})
+						    		}
+						    	});
+						    }
+						})
 					}
-				});
+				} );
 			}
 		});
 	}
 	this.showAgree = function() {
-		$('.agree, .logo, .cta').velocity('fadeIn', {
+		$('.agree, .logo').velocity('fadeIn', {
 			delay: 2000,
 			duration: 500,
 			complete: function() {
 				that.showInfo1();
+
 			}
 		});
+		$('.cta').velocity({ 
+		  right: "0px"
+		},{
+			complete:function(){ 
+				$('.cta .cta-arrow').addClass('animated bounce');
+			}
+
+		}, 1000);
 	}
 	this.showInfo1 = function() {
 		$('.headline, .agree').velocity('fadeOut', {
@@ -40,6 +73,7 @@ var App = function() {
 					duration: 500,
 					complete: function() {
 						that.showInfo2();
+						
 					}
 				});
 			}
