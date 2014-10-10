a86ed6440cb7e01d2e854a692eb4a831ff022133
Carolina <racoquita@me.com>
2014-10-10 11:47:23 -0400

    Finishing the Mobile animation for SMALL BUSINESS - Chevron

--- small-business/2014-09-30/mobile/js/app.js
+++ small-business/2014-09-30/mobile/js/app.js
@@ -8,94 +8,101 @@ var App = function() {
 		$('#qzad').velocity({
 			backgroundPosition: '0%'
 		}, {
-			delay: 1000,
-			duration: 1000,
+			easing:"linear",
+			duration: 2750,
 			complete: function() {
-				$('.headline').velocity('fadeIn', {
-					delay: 0,
-					duration: 500,
-					complete: function() {
-						
-						//that.showAgree();
-						$('.headline').velocity({ height: "+=85px" },{ 
-						    delay: 100,
-						    duration: 500,
-						    complete:function(){
-
-						    	$('.headline').velocity({ height: "+=122px" },{
-						    		delay:200,
-						    		duration: 500,
-						    		complete:function(){
-						    			//that.showAgree();
-						    			$('.headline div img').velocity({
-						    				marginTop:"0px"
-						    			}, {
-						    				delay: 100,
-						    				duration: 500,
-						    				complete:function() {
-						    					that.showAgree();
-						    				}
-						    			})
-						    		}
-						    	});
-						    }
-						})
-					}
-				} );
+				
 			}
 		});
+		$('.headline').velocity({
+			left: "0px"
+		}, {
+			duration:500,
+			easing:"linear",
+			delay: 1000,
+			complete:function () {
+				$('.headline div.needs img').velocity({
+    				marginTop:"0px"
+    			}, {
+    				delay: 1,
+    				duration: 500,
+    				easing: "easeOutSine",
+    				complete:function() {
+    					$('.headline div.sm-bus img').velocity({
+    						marginTop:"0px"
+    					}, {
+    						delay:1,
+    						duration:500,
+    						easing: "easeOutSine",
+    						complete:function () {
+    							that.showAgree();
+    						}
+    					})
+    					
+    				}
+    			})
+			
+			}
+		})
 	}
 	this.showAgree = function() {
-		$('.agree, .logo').velocity('fadeIn', {
+		$('.agree img, .logo').velocity({
+			scale: 3
+		}, {
+			duration: 0,
+			complete: function() {
+				$('.agree img, .logo').velocity({
+					scale: 1,
+					opacity: 1
+				}, {
+					duration: 500,
+					delay: 750,
+					complete: function() {
+						that.hideHeadline();
+						that.showInfo1();
+					}
+				});
+
+			}
+		});
+		
+	}
+	this.hideHeadline = function () {
+		$('.headline, .agree').velocity({
+			left: "-100%"
+		}, {
+			delay: 2000,
+			duration: 500
+		});
+	}
+	this.showInfo1 = function() {
+		
+		$('.info-1').velocity({
+			left:0
+		}, {
 			delay: 2000,
 			duration: 500,
 			complete: function() {
-				that.showInfo1();
+				//that.showInfo2();
+				$('.cta').velocity({ 
+				  right: "0px"
+				},{
+					delay:500,
+					duration:500,
+					complete:function(){ 
+						$('.cta .cta-arrow').addClass('animated bounce');
+					}
 
+				}, 1000);
 			}
 		});
-		$('.cta').velocity({ 
-		  right: "0px"
-		},{
-			complete:function(){ 
-				$('.cta .cta-arrow').addClass('animated bounce');
-			}
-
-		}, 1000);
+					
 	}
-	this.showInfo1 = function() {
-		$('.headline, .agree').velocity('fadeOut', {
-			delay: 3000,
-			duration: 500,
-			complete: function() {
-				$('.info-1').velocity('fadeIn', {
-					delay: 0,
-					duration: 500,
-					complete: function() {
-						that.showInfo2();
-						
-					}
-				});
-			}
-		});				
-	}
-	this.showInfo2 = function() {
-		$('.info-1').velocity('fadeOut', {
-			delay: 3000,
-			duration: 500,
-			complete: function() {
-				$('.info-2').velocity('fadeIn', {
-					delay: 0,
-					duration: 500,
-					complete: function() {
-						
-					}
-				});
-			}
-		});
-	}
+	
 	this.off = function() {
-		$('#qzad').css('background-position', '100%');
-		$('#qzad > div').hide();
+		// $('#qzad').css('background-position', '100%');
+		// $('#qzad > div').hide();
+		$('.cta .cta-arrow').removeClass('animated bounce');
+		$('#qzad, .headline, .agree, .agree img, .info-1, .cta, .logo, .needs img, .sm-bus img').removeAttr('style')
 	}
 };
